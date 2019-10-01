edc54e57d1429382778b821b707dedec745a16de
Pat Dunlavey <pat@born-digital.com>
2019-10-01 16:04:43 -0400

    pat / master --- test changing foxmlToSolr.xslt as a hard link. This change to foxmlToSolr.xslt is meant to be reversed after this test.

--- install.sh
+++ install.sh
@@ -8,10 +8,11 @@ mv /usr/local/solr/collection1/conf/schema.xml /home/islandora/archive_oct2019_s
 mv /var/lib/tomcat7/webapps/fedoragsearch/WEB-INF/classes/fgsconfigFinal/index/FgsIndex/foxmlToSolr.xslt /home/islandora/archive_oct2019_solr_patch/foxmlToSolr.xslt
 mv /var/lib/tomcat7/webapps/fedoragsearch/WEB-INF/classes/fgsconfigFinal/index/FgsIndex/islandora_transforms /home/islandora/archive_oct2019_solr_patch/islandora_transforms
 
-echo "Copying updated files"
+echo "Linking updated files"
 ln -s /opt/compass-solr-config/solr/schema.xml /usr/local/solr/collection1/conf/schema.xml
-ln -s /opt/compass-solr-config/fedora/gsearch/foxmlToSolr.xslt /var/lib/tomcat7/webapps/fedoragsearch/WEB-INF/classes/fgsconfigFinal/index/FgsIndex/foxmlToSolr.xslt
 ln -s /opt/compass-solr-config/fedora/gsearch/islandora_transforms /var/lib/tomcat7/webapps/fedoragsearch/WEB-INF/classes/fgsconfigFinal/index/FgsIndex/islandora_transforms
+# GSearch does not like foxmlToSolr.xslt to be a soft link, so we create a hard link instead.
+ln /opt/compass-solr-config/fedora/gsearch/foxmlToSolr.xslt /var/lib/tomcat7/webapps/fedoragsearch/WEB-INF/classes/fgsconfigFinal/index/FgsIndex/foxmlToSolr.xslt
 
 echo "Change permissions on updated files"
 chown -h tomcat7:tomcat7 /usr/local/solr/collection1/conf/schema.xml
